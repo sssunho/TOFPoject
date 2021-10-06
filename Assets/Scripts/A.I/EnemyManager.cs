bb0ac801379940b529b6ae46b98e0dc98823deaa
Susu-kr <57660360+Susu-kr@users.noreply.github.com>
2021-10-06 20:03:21 +0900

    update enemy scripts

---
+++ Assets/Scripts/A.I/EnemyManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TOF
+{
+    public class EnemyManager : CharacterManager
+    {
+        EnemyLocomotionManager enemyLocomotionManager;
+        public bool isPerformingAction;
+
+        [Header("A.I Settings")]
+        public float detectionRadius = 20;
+        // The higher, and lower, respectively these angles are, the greater detection FIELD OF VIEW (basically like eye sight)
+        public float maximumDetectionAngle = 50;
+        public float minimumDetectionAngle = -50;
+
+        private void Awake()
+        {
+            enemyLocomotionManager = GetComponent<EnemyLocomotionManager>();
+        }
+
+        private void Update()
+        {
+
+        }
+        
+        private void FixedUpdate()
+        {
+            HandleCurrentAction();
+        }
+
+        private void HandleCurrentAction()
+        {
+            if(enemyLocomotionManager.currentTarget == null)
+            {
+                enemyLocomotionManager.HandleDetection();
+            }
+            else
+            {
+                enemyLocomotionManager.HandleMoveToTarget();
+            }
+        }
+    }
+}
+
