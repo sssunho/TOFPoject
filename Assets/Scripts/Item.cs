a9c124450e085b8f6142fbae1c7cf3916debaf09
sssunho <tvision3@gmail.com>
2021-10-06 14:03:54 +0900

    Add : Item, Item Slot

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TOF
+{
+    public class Item : ScriptableObject
+    {
+        [Header("Item Information")]
+        public Sprite itemIcon;
+        public string itemName;
+    }
+}
