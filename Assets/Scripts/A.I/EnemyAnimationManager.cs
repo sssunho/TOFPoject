5d327ed9840f59e8c50bffc9111fa423ca0f54db
Susu-kr <57660360+Susu-kr@users.noreply.github.com>
2021-10-08 15:57:04 +0900

    update Assets

---
+++ Assets/Scripts/A.I/EnemyAnimationManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TOF
+{
+    public class EnemyAnimationManager : AnimatorManager
+    {
+        EnemyManager enemyManager;
+
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+            enemyManager = GetComponentInParent<EnemyManager>();
+        }
+
+        private void OnAnimatorMove()
+        {
+            float delta = Time.deltaTime;
+            enemyManager.enemyRigidBody.drag = 0;
+            Vector3 deltaPosition = anim.deltaPosition;
+            deltaPosition.y = 0;
+            Vector3 velocity = deltaPosition / delta;
+            enemyManager.enemyRigidBody.velocity = velocity;
+        }
+    }
+}
+
