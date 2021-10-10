7b5fe49dc8101f84c536a329fbc45c7b6cc78e02
sssunho <tvision3@gmail.com>
2021-10-11 01:00:40 +0900

    Update WeaponSlotManager.cs

--- Assets/Scripts/WeaponSlotManager.cs
+++ Assets/Scripts/WeaponSlotManager.cs
@@ -10,8 +10,8 @@ namespace TOF
         PlayerManager playerManager;
 
         public WeaponItem attackingWeapon;
-        WeaponHolderSlot leftHandSlot;
-        WeaponHolderSlot rightHandSlot;
+        public WeaponHolderSlot leftHandSlot;
+        public WeaponHolderSlot rightHandSlot;
         WeaponHolderSlot backSlot;
 
         DamageCollider leftHandDamageCollider;
